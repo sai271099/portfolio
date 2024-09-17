fec637c6c3303539fef88d5d3c0d01670ea8e7c6
sai <saiyada19999@gmail.com>
2024-09-18 00:42:04 +0530

    background modified

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,19 +6,10 @@ import Contact from '@/components/Contact';
 import About from '@/components/About';
 import Projects from '@/components/Projects';
 import Skills from '@/components/Skills';
-import dynamic from 'next/dynamic';
-const Particles = dynamic(() => import('@/components/Particles'), { ssr: false });
 
 export default function Home() {
   return (
-    <div style={{ position: 'relative', overflow: 'hidden' }}>
-      {/* Particles in the background */}
-      <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', zIndex: -1 }}>
-        <Particles />
-      </div>
-
-      {/* Main content */}
-      <div style={{ position: 'relative', zIndex: 1 }}>
+    <div>
         <Title />
         <About />
         <Skills />
@@ -27,7 +18,7 @@ export default function Home() {
         <Education />
         <Contact />
         <Footer />
-      </div>
-    </div>
+        </div>
+      
   );
 }
